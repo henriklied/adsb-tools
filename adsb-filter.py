b872b5cb8061e971a6760caedae542e6bed7f7b8
Henrik Lied <henriklied@gmail.com>
2017-08-25 15:24:19 +0200

    You can now download data from a start date and until n days

--- adsb-filter.py
+++ adsb-filter.py
@@ -4,6 +4,7 @@ import os, sys
 import argparse
 import zipfile
 import uuid
+import datetime
 
 import requests, geojson
 
@@ -12,53 +13,70 @@ from clint.textui import progress
 
 parser = argparse.ArgumentParser(description='Download daily flight data from ADSBExchange.org and return a json file with flights within a bounding box')
 parser.add_argument('--bounds', dest='bounds', default="bounds.geojson", help='Path to the geojson file that stores the bounds (default: bounds.geojson)')
-parser.add_argument('--date', dest='date', default="2017-01-01", help='What date do you want to download? E.g. 2017-01-01 (default)')
-parser.add_argument('--delete-after', dest="delete_after", action='store_true', help='Use this if you want the script to delete the 10gb-ish file after processing is complete' )
+parser.add_argument('--date', dest='date', default="2017-01-29", help='What date do you want to download? Y-m-d, e.g. 2017-01-29 (default)')
+parser.add_argument('--range', dest='days_ahead', default=1, help="If specified, this will download flight data for n days after the specified start date.")
 
 args = parser.parse_args()
 
-bounds_fp = geojson.load(open(args.bounds))
 
-coords = bounds_fp['features'][0]['geometry']['coordinates'][0]
-shape = MultiPoint(coords).convex_hull
+class FlightParser(object):
+  def __init__(self, args):
+    self.bounds = args.bounds
+    self.date = args.date
+    self.days_ahead = int(args.days_ahead)
+    self.bounds_fp = geojson.load(open(args.bounds))
+    self.coords = self.bounds_fp['features'][0]['geometry']['coordinates'][0]
+    self.shape = MultiPoint(self.coords).convex_hull
+    self.out_tmpfile = "%s.json" % str(uuid.uuid4())
+    self.inside = []
 
-# Where the zip file is saved
-temp_file = '/tmp/adsb_%s.zip' % (args.date)
+  def run(self):
+    if self.days_ahead == 1:
+      self.parse(self.date)
+    else:
+      year, month, day = self.date.split('-')
+      for date in self.find_dates(int(year), int(month), int(day)):
+        self.parse(date)
+    with open(self.out_tmpfile, 'w') as f:
+      f.write(json.dumps(inside))
+    print 'Filtered data is now in %s' % self.out_tmpfile
 
-# Local file pointer for the filtered positions
-out_tmpfile = '%s.json' % str(uuid.uuid4())
+  def find_dates(self, year, month, day):
+    base = datetime.datetime(year, month, day)
+    for obj in [base + datetime.timedelta(days=x) for x in range(0, self.days_ahead)]:
+      yield obj.strftime('%Y-%m-%d')
 
-# Array which holds the flight positions within the bounding box
-inside = []
+  def parse(self, date):
+    print 'Downloading http://history.adsbexchange.com/Aircraftlist.json/%s.zip' % date
+    temp_file = '/tmp/adsb_%s.zip' % date
+    if not os.path.exists(temp_file):
+      r = requests.get('http://history.adsbexchange.com/Aircraftlist.json/%s.zip' % date, stream=True)
+      with open(temp_file, 'wb') as f:
+        total_length = int(r.headers.get('content-length'))
+        for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
+          if chunk:
+            f.write(chunk)
+            f.flush()
+    self.filter(temp_file, date)
+
+  def filter(self, f, date):
+    out_tmpfile = 'filtered_%s.json' % date
+    zf = zipfile.ZipFile(f)
+    for info in zf.infolist():
+      try:
+        data = json.loads(zf.read(info.filename))
+        print "Working on", info.filename
+        for sample in data['acList']:
+          if 'Lat' in sample and 'Long' in sample and 'Icao' in sample:
+            point = Point([sample['Long'], sample['Lat']])
+            if self.shape.contains(point):
+              self.inside.append(sample)    
+      except Exception as e:
+        print "Could not read %s from ZIP archive" % info.filename
+        print "Error:", e
+
+    os.remove(f)
 
 
-print 'Downloading http://history.adsbexchange.com/Aircraftlist.json/%s.zip' % args.date
-if not os.path.exists(temp_file):
-  r = requests.get('http://history.adsbexchange.com/Aircraftlist.json/%s.zip' % args.date, stream=True)
-  with open(temp_file, 'wb') as f:
-    total_length = int(r.headers.get('content-length'))
-    for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
-      if chunk:
-        f.write(chunk)
-        f.flush()
-
-zf = zipfile.ZipFile(temp_file)
-for info in zf.infolist():
-  try:
-    data = json.loads(zf.read(info.filename))
-    print "Working on", info.filename
-    for sample in data['acList']:
-      if 'Lat' in sample and 'Long' in sample and 'Icao' in sample:
-        point = Point([sample['Long'], sample['Lat']])
-        if shape.contains(point):
-          inside.append(sample)    
-  except:
-    print "Could not read %s from ZIP archive" % info.filename
-
-with open(out_tmpfile, 'w') as f:
-  f.write(json.dumps(inside))
-
-print 'Filtered positions are in %s' % out_tmpfile
-
-if args.delete_after:
-  os.remove(temp_file)
+p = FlightParser(args)
+p.run()
